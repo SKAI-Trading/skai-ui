cebe7a28d69eaf7e4a9a59182ec1b0a80cc8879d
skaicasey <casey@skai.trade>
2026-02-02 02:14:59 -0700

    fix: use correct type exports from react-resizable-panels
    PanelGroupProps -> GroupProps
    PanelResizeHandleProps -> SeparatorProps

--- src/components/layout/resizable.tsx
+++ src/components/layout/resizable.tsx
@@ -24,14 +24,15 @@ import {
   Panel,
   Group as PanelGroup,
   Separator as PanelResizeHandle,
+  type GroupProps,
+  type SeparatorProps,
 } from "react-resizable-panels";
-import type { PanelGroupProps, PanelResizeHandleProps } from "react-resizable-panels";
 import { cn } from "../../lib/utils";
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: PanelGroupProps & { className?: string }) => (
+}: GroupProps & { className?: string }) => (
   <PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
@@ -46,7 +47,7 @@ const ResizablePanel = Panel;
 const ResizableHandle = ({
   className,
   ...props
-}: PanelResizeHandleProps & { className?: string }) => (
+}: SeparatorProps & { className?: string }) => (
   <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-white/5 after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
