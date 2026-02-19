f3a789ea9ad9bfc9268b8d57b028974813a7c430
skaicasey <casey@skai.trade>
2026-02-19 08:45:10 -0700

    fix: update react-resizable-panels imports for v3 API

--- src/components/layout/resizable.tsx
+++ src/components/layout/resizable.tsx
@@ -22,17 +22,16 @@
 
 import {
   Panel,
-  Group as PanelGroup,
-  Separator as PanelResizeHandle,
-  type GroupProps,
-  type SeparatorProps,
+  PanelGroup,
+  PanelResizeHandle,
 } from "react-resizable-panels";
+import type { ComponentProps } from "react";
 import { cn } from "../../lib/utils";
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: GroupProps & { className?: string }) => (
+}: ComponentProps<typeof PanelGroup>) => (
   <PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
@@ -47,7 +46,7 @@ const ResizablePanel = Panel;
 const ResizableHandle = ({
   className,
   ...props
-}: SeparatorProps & { className?: string }) => (
+}: ComponentProps<typeof PanelResizeHandle>) => (
   <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-white/5 after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
